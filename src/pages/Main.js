2d7f8ace88c5300b3bbb3c22db980e3c1c00524b
ehfdl <cmaw@naver.com>
2022-12-15 09:36:58 +0900

    no message

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,6 +4,9 @@ import InputSec from "../components/InputSec";
 import Search from "../components/Search";
 import Listbox from "../components/Listbox";
 
+// Input = id: new Date(), input div 사용
+// InputSec = id: uuidv4(), input form 사용
+
 function Main() {
   return (
     <div>
